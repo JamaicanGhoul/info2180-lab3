6e907965e153e5eb634c15a03fe0575fe496d7a5
Javier Stewart <javierlamarstewart68@gmail.com>
2020-10-11 14:37:27 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,8 @@ window.onload = event =>{
 function gamePlay(){
     let board = document.getElementById('board');
     let squares = board.getElementsByTagName('div');
+    let status = document.getElementById('status');
+    let restart = document.getElementsByClassName('btn')[0];
     let index = 0;
     let currentHolder = new Array(9); 
     let ender = 'O';
@@ -32,6 +34,48 @@ function gamePlay(){
                 currentHolder[event.target.id] = 'X';
                 ender = 'X';
             }
+
+            if ( currentHolder[0] !== undefined && ((currentHolder[0]===currentHolder[1] && currentHolder[1]===currentHolder[2]) 
+                || (currentHolder[0]===currentHolder[3] && currentHolder[3]===currentHolder[6]) 
+                || (currentHolder[0]===currentHolder[4] && currentHolder[4]===currentHolder[8]) )){  
+                
+                status.innerText = `Congratulations! ${currentHolder[0]} is the Winner!`;
+                status.classList.add('you-won');
+                isGameEnd = true;
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }
+            
+            else if (currentHolder[4] !== undefined && ((currentHolder[1]===currentHolder[4] && currentHolder[4]===currentHolder[7]) 
+                || (currentHolder[3]===currentHolder[4] && currentHolder[4]===currentHolder[5])
+                || (currentHolder[2]===currentHolder[4] && currentHolder[4]===currentHolder[6]) )){
+                
+                status.innerText = `Congratulations! ${currentHolder[4]} is the Winner!`;
+                status.classList.add('you-won');
+                isGameEnd = true;
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }
+
+            else if( currentHolder[8] !== undefined && ((currentHolder[6]===currentHolder[7] && currentHolder[7]===currentHolder[8]) 
+                || (currentHolder[2]===currentHolder[5] && currentHolder[5]===currentHolder[8]))){
+                    
+                status.innerText = `Congratulations! ${currentHolder[8]} is the Winner!`;
+                status.classList.add('you-won');
+                isGameEnd = true;
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }
+
         };
 
         square.onmouseover = event =>{
