52b131505c41d012e577ae9377b94996feeff90f
Javier Stewart <javierlamarstewart68@gmail.com>
2020-10-11 14:35:38 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,9 +6,42 @@ function gamePlay(){
     let board = document.getElementById('board');
     let squares = board.getElementsByTagName('div');
     let index = 0;
+    let currentHolder = new Array(9); 
+    let ender = 'O';
+    let isGameEnd = false;
 
     for (let square of squares){
         square.classList.add('square');
         square.id = index ++; 
-    } 
+        
+        square.onclick = (event)=>{
+
+            if (currentHolder[event.target.id] || isGameEnd){
+                return;
+            }
+
+            if (ender === 'X'){
+                event.target.innerText = 'O';
+                event.target.classList.add('O');
+                currentHolder[event.target.id] = 'O';
+                ender = 'O';
+            }
+            else if (ender === 'O'){
+                event.target.innerText = 'X';
+                event.target.classList.add('X');
+                currentHolder[event.target.id] = 'X';
+                ender = 'X';
+            }
+        };
+
+        square.onmouseover = event =>{
+            if(!isGameEnd){
+                event.target.classList.add('hover');
+            }
+        };
+
+        square.onmouseleave = event =>{
+            event.target.classList.remove('hover');
+        };
+    }
 }
